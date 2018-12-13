a2752513568994ed6246c71acac94b485c894022
Satu <satu.suominen@digia.com>
2018-12-13 10:06:01 +0200

    Fix bug with sessions uid variable

--- src/services/populateUserData.ts
+++ src/services/populateUserData.ts
@@ -3,7 +3,7 @@ const authService = require("./authService");
 module.exports = (req: Request, res: Response, next: NextFunction) => {
     if (!req.session.userData) {
     req.session.userData = authService.getUserData(req.headers);
-    req.session.userData.uid = authService.parseDomainFromHeadersData(req.headers["shib-uid"]);
+    req.session.userData.uid = req.headers["shib-uid"];
     req.session.userData.ip = req.headers["x-forwarded-for"] || (req.connection && req.connection.remoteAddress) || "";
     next();
     }
