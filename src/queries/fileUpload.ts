b7a5bab0f5d3a2f730eb4e71304ba41d20d808f5
Satu <satu.suominen@digia.com>
2018-12-28 15:44:26 +0200

    Refactor file upload file and rename all files as file.blob

--- src/queries/fileUpload.ts
+++ src/queries/fileUpload.ts
@@ -9,74 +9,95 @@ const upload = multer({ dest: "temp/" });
 
 // Database connection
 const connection = require("./../db");
+const dbHelpers = require("./../databaseHelpers");
 
 const publicationFolder = "publications";
+const savedFileName = "file.blob";
+
 
 async function uploadJulkaisu(req: Request, res: Response) {
     upload.single("file") (req, res, async function () {
 
-        const julkaisuid = req.body.data.id;
+        const julkaisuId = req.body.data.julkaisuid;
+        const julkaisuData = req.body.data;
         const file = (<any>req).file;
 
         const valid = await validate(file.originalname, file.path);
 
-        // if (!valid) {
-        //     return res.status(403).send( "Invalid file" );
-        // } else {
-        //     return res.status(200).send("File uploaded succesfully");
-        // }
-
-        const fileMoved = await moveFile(file, julkaisuid);
-
-        if (fileMoved) {
-            await updateHelperTables(file, julkaisuid);
+        if (!valid) {
+            return res.status(403).send( "Invalid file" );
         } else {
-            // if file transfer leads to error, delete file from temp folder
-            fs.unlinkSync(file.path);
-            return res.status(500).send( "Failure in file transfer" );
+            const fileMoved = await moveFile(file, julkaisuId);
+            if (fileMoved) {
+                await updateQueueTable(file, julkaisuId);
+                await updateArchiveTable(file, julkaisuData);
+
+                return res.status(200).json("OK");
+            } else {
+                // if file transfer leads to error, delete file from temp folder
+                fs.unlinkSync(file.path);
+                return res.status(500).send( "Failure in file transfer" );
+            }
         }
 
     });
 
 }
 
-async function updateHelperTables(file: any, julkaisuid: any) {
+async function updateArchiveTable(file: any, data: any) {
 
-    const julkaisuColumns = new connection.pgp.helpers.ColumnSet(["julkaisuid"], {table: "julkaisujono"});
-    const query = connection.pgp.helpers.insert({"julkaisuid": julkaisuid }, julkaisuColumns) + "RETURNING id";
+    const tableColumns = dbHelpers.julkaisuarkisto;
 
-    const jonoid = await connection.db.one(query);
-    console.log(jonoid);
+    if (data.embargo === "" ) {
+        data["embargo"] = undefined;
+    }
+
+    data["filename"] = file.originalname;
+    data["mimetype"] = file.mimetype;
+
+    const table = new connection.pgp.helpers.ColumnSet(tableColumns, {table: "julkaisuarkisto"});
+    const query = connection.pgp.helpers.insert(data, table) + "RETURNING id";
+
+    connection.db.one(query);
 
 }
 
+async function updateQueueTable(file: any, julkaisuid: any) {
+
+    try {
+        const tableColumns = new connection.pgp.helpers.ColumnSet(["julkaisuid"], {table: "julkaisujono"});
+        const query = connection.pgp.helpers.insert({"julkaisuid": julkaisuid }, tableColumns) + "RETURNING id";
+        await connection.db.one(query);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 async function moveFile (file: any, id: any) {
 
    let done: boolean = true;
-
-    const publicationDir = publicationFolder + "/" + id;
+   const publicationDir = publicationFolder + "/" + id;
 
     try {
-        // create folder for publications, for identifying we put file to folder which name is publication id
-        fs.mkdirSync(publicationDir, function (err: any) {
-            // if (err) throw err;
-        });
-
-        // after creating folder we move publication to that folder
-        const oldPath = file.path;
-        const newPath = publicationDir + "/" + file.filename;
-
-        // TODO: rename also filename (etc file.blob)
-        fs.rename(oldPath, newPath, function (err: any) {
-            // if (err) throw err;
-            console.log("Successfully moved publication");
-        });
-
+          fs.mkdirSync(publicationDir);
     } catch (e) {
-        console.log(e);
+
+        if (e.code === "EEXIST") {
+        //    remove old file
+        }
+        // TODO: catch error if file already exists
         done = false;
     }
+        // after creating folder we move publication to that folder
+        const oldPath = file.path;
+        const newPath = publicationDir + "/" + savedFileName;
+
+        try {
+             fs.rename(oldPath, newPath);
+        } catch (e) {
+            console.log(e);
+            done = false;
+        }
 
     return done;
 }
