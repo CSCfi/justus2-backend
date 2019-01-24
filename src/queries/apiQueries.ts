06786fd139a226a1a6e8a70703b489434956f146
ViciGallen <gallen.victor@gmail.com>
2019-01-24 13:13:09 +0200

    Session initializing location changed, push for testing

--- src/queries/apiQueries.ts
+++ src/queries/apiQueries.ts
@@ -185,6 +185,9 @@ function getUser(req: Request, res: Response, next: NextFunction) {
         return res.status(401).send("Unauthorized");
     }
     else {
+        req.session.userData = authService.getUserData(req.headers);
+        req.session.userData.uid = req.headers["shib-uid"];
+        req.session.userData.ip = req.headers["x-forwarded-for"] || (req.connection && req.connection.remoteAddress) || "";
         oh.ObjectHandlerUser(userData, function(result: any) {
             res.status(200).json(
                 result
