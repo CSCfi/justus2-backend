63bbcf25e8ecb47d88968ddde1a578838c6f3171
Satu <satu.suominen@digia.com>
2019-01-30 16:57:00 +0200

    Use middleware for storing user data to session

--- src/services/populateUserData.ts
+++ src/services/populateUserData.ts
@@ -1,8 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 const authService = require("./authService");
 module.exports = (req: Request, res: Response, next: NextFunction) => {
-    req.session.userData = authService.getUserData(req.headers);
-    req.session.userData.uid = req.headers["shib-uid"];
-    req.session.userData.ip = req.headers["x-forwarded-for"] || (req.connection && req.connection.remoteAddress) || "";
-    next();
+
+    if (!authService.getUserData(req.headers)) {
+        next();
+    } else {
+        req.session.userData = authService.getUserData(req.headers);
+        req.session.userData.uid = req.headers["shib-uid"];
+        if (!req.session.language) {
+            req.session.language = "FI";
+        }
+        req.session.userData.ip = req.headers["x-forwarded-for"] || (req.connection && req.connection.remoteAddress) || "";
+        next();
+    }
 };
