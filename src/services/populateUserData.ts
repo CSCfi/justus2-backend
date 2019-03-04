6dece5ce3130ce41f9c6c5818e2cba9239f4cd25
Satu <satu.suominen@digia.com>
2019-03-04 12:38:26 +0200

    Debugging with public routes

--- src/services/populateUserData.ts
+++ src/services/populateUserData.ts
@@ -3,13 +3,14 @@ const authService = require("./authService");
 module.exports = (req: Request, res: Response, next: NextFunction) => {
 
     console.log("populate user data");
-    console.log(req.originalUrl);
-    console.log(req.headers);
+    // console.log(req.originalUrl);
+    // console.log(req.headers);
 
     // return data from this route before user is authenticated
     if (req.originalUrl === "/organisaationimet") {
+        console.log("Organisaationimet is original url");
         console.log(req.originalUrl);
-        next();
+        return next();
     }
 
     if (!authService.getUserData(req.headers)) {
