8a879cc2d29aac62fcb87a0c56c7f16ca862de7b
Satu <satu.suominen@digia.com>
2019-04-12 12:42:14 +0300

    Fixed typo

--- sql/001_tables.sql
+++ sql/001_tables.sql
@@ -236,7 +236,7 @@ CREATE TABLE julkaisuarkisto
   embargo timestamp with time zone,
   oikeudet character varying,
   versio character varying(1),
-  julkaisuarkisto character varying,
+  julkaisusarja character varying,
   CONSTRAINT julkaisuarkisto_pkey PRIMARY KEY (id),
   CONSTRAINT fk_julkaisujono FOREIGN KEY (julkaisuid)
       REFERENCES julkaisu (id) MATCH SIMPLE
