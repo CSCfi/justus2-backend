5b74359db4dde7691f91ecbc2e654f490f2a675d
Satu <satu.suominen@digia.com>
2019-04-18 12:54:55 +0300

    Remove organization contact info

---
+++ src/organization_contacts.ts
@@ -0,0 +1,254 @@
+const contactList = [
+    {
+        "domain": "@csc.fi",
+        "code": "00000",
+        "email": "notvalid@csc.fi",
+        "showPublicationInput": true
+    },
+    {
+        "domain": "@digia.com",
+        "code": "00000",
+        "email": "notvalid@digia.com",
+        "showPublicationInput": true
+    },
+    {
+        // Arcada - Nylands svenska yrkeshögskola  #arcada-admins
+        "domain": "@arcada.fi",
+        "code": "02535",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Centria-ammattikorkeakoulu  #centria-admins
+        "domain": "@centria.fi",
+        "code": "02536",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Diakonia-ammattikorkeakoulu  #diak-admins
+        "domain": "@diak.fi",
+        "code": "02623",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Haaga-Helia ammattikorkeakoulu  #haaga-helia-admins
+        "domain": "@haaga-helia.fi",
+        "code": "10056",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Humanistinen ammattikorkeakoulu  #humak-admins
+        "domain": "@humak.fi",
+        "code": "02631",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Hämeen ammattikorkeakoulu
+        "domain": "@hamk.fi",
+        "code": "02467",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Jyväskylän ammattikorkeakoulu  #jamk-admins
+        "domain": "@jamk.fi",
+        "code": "02504",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Kajaanin ammattikorkeakoulu  #kamk-admins
+        "domain": "@kamk.fi",
+        "code": "02473",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Karelia-ammattikorkeakoulu  #karelia-admins
+        "domain": "@karelia.fi",
+        "code": "02469",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // nb! xamk may have 3 domains (mahd. kyamk.fi ja mamk.fi)
+        // Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+        "domain": "@xamk.fi",
+        "code": "10118",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+        "domain": "@kyamk.fi",
+        "code": "10118",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Kaakkois-Suomen ammattikorkeakoulu  #xamk-admins
+        "domain": "@mamk.fi",
+        "code": "10118",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Lahden ammattikorkeakoulu  #lamk-admins
+        "domain": "@lamk.fi",
+        "code": "02470",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Laurea-ammattikorkeakoulu  #laurea-admins
+        "domain": "@laurea.fi",
+        "code": "02629",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Metropolia ammattikorkeakoulu  #metropolia-admins
+        "domain": "@metropolia.fi",
+        "code": "10065",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Satakunnan ammattikorkeakoulu  #samk-admins
+        "domain": "@samk.fi",
+        "code": "02507",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Seinäjoen ammattikorkeakoulu  #seamk-admins
+        "domain": "@seamk.fi",
+        "code": "02472",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Tampereen ammattikorkeakoulu  #tamk-admins
+        "domain": "@tamk.fi",
+        "code": "02630",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Tampereen ammattikorkeakoulu  #tamk-admins
+        "domain": "@tuni.fi",
+        "code": "02630",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Yrkeshögskolan Novia  #novia-admins
+        "domain": "@novia.fi",
+        "code": "10066",
+        "email": "",
+        "showPublicationInput": true
+
+    },
+    {
+        // Poliisiammattikorkeakoulu  #polamk-admins
+        "domain": "@polamk.fi",
+        "code": "02557",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Poliisiammattikorkeakoulu
+        "domain": "@poliisi.fi",
+        "code": "02557",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // tutkimusorganisaatio
+        // Ilmatieteen laitos  #fmi-admins
+        "domain": "@fmi.fi",
+        "code": "4940015",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // nb! mml has 2 domains
+        // Maanmittauslaitos  #mml-admins
+        "domain": "@nls.fi",
+        "code": "4020217",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Maanmittauslaitos  #mml-admins
+        "domain": "@maanmittauslaitos.fi",
+        "code": "4020217",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Maanpuolustuskorkeakoulu
+        "domain": "@mil.fi",
+        "code": "02358",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Savonia-ammattikorkeakoulu
+        "domain": "@savonia.fi",
+        "code": "02537",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Turun ammattikorkeakoulu
+        "domain": "@turkuamk.fi",
+        "code": "02509",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Taideyliopisto
+        "domain": "@uniarts.fi",
+        "code": "10103",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Luonnonvarakeskus
+        "domain": "@luke.fi",
+        "code": "4100010",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // Oulun ammattikorkeakoulu #oamk-admins
+        "domain": "@oamk.fi",
+        "code": "02471",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // 	Geologian tutkimuskeskus #gtk-admins
+        "domain": "@gtk.fi",
+        "code": "5040011",
+        "email": "",
+        "showPublicationInput": true
+    },
+    {
+        // 	Terveyden ja hyvinvoinnin laitos #thl-admins
+        "domain": "@thl.fi",
+        "code": "5610017",
+        "email": "",
+        "showPublicationInput": true
+    }
+];
+
+module.exports = {
+    domainMappings: contactList
+
+};
